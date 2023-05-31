84efb178611b21690db0bb1ec0ab6d3a1ae404be
Zhao Ji <693941639@qq.com>
2023-05-31 17:58:27 +0800

    concurrent process test

--- concurrentProcess.py
+++ concurrentProcess.py
@@ -3,9 +3,13 @@ import concurrent.futures
 def test(n):
     return sum(i * i for i in range(n))
 
+numbers = [10000000 + x for x in range(20)]
+
 def main():
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(test, [1, 2, 3])
+        rel = executor.map(test, numbers)
+
+    print(list(rel))
 
 if __name__ == '__main__':
    main()
